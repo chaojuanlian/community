164a37040f350a17ef1b10ef9904f8467b2e7fe6
wangyuchao <chaojuanlian@163.com>
2019-11-13 21:59:10 +0800

    add home

--- src/main/java/life/majiang/community/demo/controller/IndexController.java
+++ src/main/java/life/majiang/community/demo/controller/IndexController.java
@@ -1,8 +1,12 @@
 package life.majiang.community.demo.controller;
 
 
+import life.majiang.community.demo.dto.QuestionDTO;
+import life.majiang.community.demo.mapper.QuestionMapper;
 import life.majiang.community.demo.mapper.UserMapper;
+import life.majiang.community.demo.model.Question;
 import life.majiang.community.demo.model.User;
+import life.majiang.community.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  *
@@ -21,8 +26,12 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -37,6 +46,8 @@ public class IndexController {
 
             }
         }
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
