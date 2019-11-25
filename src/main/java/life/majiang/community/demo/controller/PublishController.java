9667d7ae9a16af089e3bfbc28bccee317c8271cb
wangyuchao <chaojuanlian@163.com>
2019-11-25 22:33:05 +0800

    add edit  and add logout

--- src/main/java/life/majiang/community/demo/controller/PublishController.java
+++ src/main/java/life/majiang/community/demo/controller/PublishController.java
@@ -1,12 +1,14 @@
 package life.majiang.community.demo.controller;
 
-import life.majiang.community.demo.mapper.QuestionMapper;
+import life.majiang.community.demo.dto.QuestionDTO;
 import life.majiang.community.demo.model.Question;
 import life.majiang.community.demo.model.User;
+import life.majiang.community.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,7 +18,18 @@ import javax.servlet.http.HttpServletRequest;
 public class PublishController {
     
     @Autowired
-    private QuestionMapper questionMapper;
+    private QuestionService questionService;
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Integer id,
+                       Model model){
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title",question.getTitle());
+        model.addAttribute("description",question.getDescription());
+        model.addAttribute("tag",question.getTag());
+        model.addAttribute("id",question.getId());
+        return "publish";
+    }
 
     @GetMapping("/publish")
     public String publish(){
@@ -28,6 +41,7 @@ public class PublishController {
             @RequestParam(value = "title",required = false) String title,
             @RequestParam(value = "description",required = false) String description,
             @RequestParam(value = "tag",required = false) String tag,
+            @RequestParam(value = "id",required = false) Integer id,
             HttpServletRequest request,
             Model model    ){
         model.addAttribute("title",title);
@@ -59,7 +73,8 @@ public class PublishController {
         question.setCreator(user.getId());
         question.setGmtCreate(System.currentTimeMillis());
         question.setGmtModified(question.getGmtCreate());
-        questionMapper.create(question);
+        question.setId(id);
+        questionService.createOrUpdate(question);
         return "redirect:/";
     }
 }
